a27562f60d65e78d00be63b0bc9a50ca7462c4b2
maendeleoperry <maendeleoperry@hotmail.com>
2019-09-19 05:56:32 +0000

    fixed

--- water.rb
+++ water.rb
@@ -1,17 +1,17 @@
 class Ocean
     attr_accessor :composition, :inhabintants
     
-    def initialize(:composition, :inhabintants)
-        @contents = composition
-        @wildlife = inhabintants
+    def initialize(composition, inhabintants)
+        @composition = composition
+        @inhabintants = inhabintants
     end  
     def output_ocean
-      puts "Oceans all have #{@contents} and #{@wildlife}"  
+      puts "Oceans all have #{@composition} and #{@inhabintants}"  
     end    
     
 end    
 
-ocean = Ocean.new(:water, :dolphins)
+ocean = Ocean.new("water", "dolphins")
 puts ocean
 puts ocean.composition
 puts ocean.inhabintants
